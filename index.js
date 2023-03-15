a9ecced4420275ea3f06a1437dec397d8df3905c
SaurabhRai19 <saurabhrai473@gmail.com>
2023-03-15 10:00:34 +0000

    My first action is ready

--- index.js
+++ index.js
@@ -4,32 +4,22 @@ const github = require('@actions/github');
 try {
     function ValidateEmail(mail) 
     {   
-        console.log(String(mail).toLowerCase());
         const res = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        console.log(res.test(String(mail).toLowerCase()));
         return res.test(String(mail).toLowerCase());
     }
-  // `who-to-greet` input defined in action metadata file
+  // `email` input defined in action metadata file
   const email = core.getInput('email');
-  console.log(`Hello ${email}!`);
-  var validornot= ValidateEmail(email);
-  console.log("Validornot Variable");
-  console.log(validornot);
-  console.log(typeof validornot);
+  console.log(`Entered email is ${email}.`);
+  var validornot= ValidateEmail(email); //validornot is of boolean type
   if(validornot === true){
     core.setOutput("message", "Valid Email and returned value is not string");
   }else if(validornot === false){
-   // core.setOutput("message", "Invalid Email and returned value is not string");
-   // core.setFailed("Invalid Email syntax");
+   core.setOutput("message", "Invalid Email and returned value is not string");
+   core.setFailed("Invalid Email syntax");
 }
-//   if(validornot == "true"){
-//     core.setOutput("message", "Valid");
-//   }else if(validornot === false){
-//     core.setOutput("message", "Invalid");
-//   }
   // Get the JSON webhook payload for the event that triggered the workflow
  // const payload = JSON.stringify(github.context.payload, undefined, 2)
   //console.log(`The event payload: ${payload}`);
 } catch (error) {
-  core.setFailed("Invalid email syntax");
+  core.setFailed(error.message);
 }
