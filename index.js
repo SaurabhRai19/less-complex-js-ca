968ee75e3f1e1278fceee1a0504e0aa32e59406d
SaurabhRai19 <saurabhrai473@gmail.com>
2023-03-15 07:51:41 +0000

    My first action is ready

--- index.js
+++ index.js
@@ -3,22 +3,33 @@ const github = require('@actions/github');
 
 try {
     function ValidateEmail(mail) 
-    {
-     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
-      {
-        return (true)
-      }
-       // alert("You have entered an invalid email address!")
-        return (false)
+    {   
+        console.log(String(email).toLowerCase());
+        return String(email)
+        .toLowerCase()
+        .match(
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+    //  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
+    //   {
+    //     return (true)
+    //   }
+    //    // alert("You have entered an invalid email address!")
+    //     return (false)
     }
   // `who-to-greet` input defined in action metadata file
   const email = core.getInput('email');
   console.log(`Hello ${email}!`);
   var validornot= ValidateEmail(email);
   if(validornot == true){
-    core.setOutput("message", "Valid Email");
+    core.setOutput("message", "Valid Email and returned value is not string");
   }else{
-    core.setOutput("message", "Invalid Email");
+    core.setOutput("message", "Invalid Email and returned value is not string");
+  }
+  if(validornot == "true"){
+    core.setOutput("message", "Valid");
+  }else{
+    core.setOutput("message", "Invalid");
   }
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
