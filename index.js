d6b9fdadf111ef0e85eadcc76aaa5460287d0578
SaurabhRai19 <saurabhrai473@gmail.com>
2023-03-15 09:35:03 +0000

    My first action is ready

--- index.js
+++ index.js
@@ -4,23 +4,17 @@ const github = require('@actions/github');
 try {
     function ValidateEmail(mail) 
     {   
-        console.log("In Function");
         console.log(String(mail).toLowerCase());
         const res = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         console.log(res.test(String(mail).toLowerCase()));
         return res.test(String(mail).toLowerCase());
-        
-    //  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
-    //   {
-    //     return (true)
-    //   }
-    //    // alert("You have entered an invalid email address!")
-    //     return (false)
     }
   // `who-to-greet` input defined in action metadata file
   const email = core.getInput('email');
   console.log(`Hello ${email}!`);
   var validornot= ValidateEmail(email);
+  console.log("Validornot Variable");
+  console.log(validornot);
   if(validornot == true){
     core.setOutput("message", "Valid Email and returned value is not string");
   }else{
