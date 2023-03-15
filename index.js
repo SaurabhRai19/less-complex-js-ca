b57f9c87a9a4aaa8251c646f1014288d20fe30cb
SaurabhRai19 <saurabhrai473@gmail.com>
2023-03-15 11:28:31 +0000

    My first action is ready

--- index.js
+++ index.js
@@ -2,6 +2,39 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
+    async function azureAPICalling(){
+        const accessToken = k5f7ugo6fuojtq7wvn6kx7d3ggvvisgqz7zr4m6mi6545ggop4zq;
+  let organization = "DevOps-MBU";
+  let project = "DemoProject";
+  let pipelineId = 78 || null;
+  let runId = 971 || null;
+  if (accessToken == null || accessToken === "") {
+    throw new Error("Please provide an access token");
+  } else {
+    console.log("token is present!");
+  }
+  let url = `https://dev.azure.com/${organization}/${project}/_apis/pipelines?api-version=7.1-preview.1&=`;
+  const request = await axios({
+    url,
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      // This!
+      Authorization: `Basic ${Buffer.from(`PAT:${accessToken}`).toString(
+        "base64"
+      )}`,
+      "X-TFS-FedAuthRedirect": "Suppress",
+    },
+  });
+  let responsedata = request.data;
+  //Print Names
+  let arrOfObj=responsedata.value;
+  arrOfObj.forEach(obj => console.log(obj.name));
+  const jsonFormat = JSON.stringify(responsedata, null, 2);
+  console.log(jsonFormat);
+
+    }
+   azureAPICalling(); 
     function ValidateEmail(mail) 
     {   
         const res = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
